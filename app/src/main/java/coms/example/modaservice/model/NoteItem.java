acf2b33554e29678c0e366b35f4f00f865a69e4f
JMcunst <xnslqjtmghaz@naver.com>
2021-05-06 20:50:20 +0900

    feat : model setup

---
+++ app/src/main/java/coms/example/modaservice/model/NoteItem.java
@@ -0,0 +1,54 @@
+package coms.example.modaservice.model;
+
+/**
+ * Scheduler Android App
+ * Created by Angga on 10/8/2015.
+ */
+public class NoteItem {
+    private int id;
+    private String title;
+    private String label;
+    private String note;
+
+    public NoteItem() {}
+
+    public NoteItem(int id, String title, String label, String note){
+        this.id = id;
+        this.title = title;
+        this.label = label;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+}
